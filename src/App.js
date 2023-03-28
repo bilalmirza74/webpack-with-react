43ab88139e66bf4107a4ced4f70a7265eaa9bdb0
bilalmirza74 <mirzamunawaralibaig4@gmail.com>
2023-03-28 13:43:40 +0530

    ..

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function App() {
+    return (
+        <div>
+            <h1>React App</h1>
+            <b>welcome  {new Date().toDateString()}</b>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos delectus voluptatem ab repellat saepe eos molestiae cupiditate, omnis doloremque, necessitatibus totam consectetur porro sint quam libero aperiam? Laborum, quaerat?</p>
+        </div>
+    );
+}
